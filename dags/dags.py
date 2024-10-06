fb6387b9571b8a22a73033073f6f11a64b2c6092
Ashu <ashuhimself@outlook.com>
2024-10-07 02:44:42 +0530

    Lib

--- dags/dags.py
+++ dags/dags.py
@@ -35,52 +35,39 @@ end = PythonOperator(
     dag=dag
 )
 
-tg1 = TaskGroup(group_id='task_group_1', dag=dag)
+with TaskGroup(group_id='task_group_1', dag=dag) as tg1:
+    task_1 = PythonOperator(
+        task_id='task_1',
+        python_callable=task_function,
+        op_kwargs={'task_name': 'Task 1'}
+    )
 
-task_1 = PythonOperator(
-    task_id='task_1',
-    python_callable=task_function,
-    op_kwargs={'task_name': 'Task 1'},
-    dag=dag
-)
+    task_2 = PythonOperator(
+        task_id='task_2',
+        python_callable=task_function,
+        op_kwargs={'task_name': 'Task 2'}
+    )
 
-task_2 = PythonOperator(
-    task_id='task_2',
-    python_callable=task_function,
-    op_kwargs={'task_name': 'Task 2'},
-    dag=dag
-)
+    task_3 = PythonOperator(
+        task_id='task_3',
+        python_callable=task_function,
+        op_kwargs={'task_name': 'Task 3'}
+    )
 
-task_3 = PythonOperator(
-    task_id='task_3',
-    python_callable=task_function,
-    op_kwargs={'task_name': 'Task 3'},
-    dag=dag
-)
+    task_4 = PythonOperator(
+        task_id='task_4',
+        python_callable=task_function,
+        op_kwargs={'task_name': 'Task 4'}
+    )
 
-task_4 = PythonOperator(
-    task_id='task_4',
-    python_callable=task_function,
-    op_kwargs={'task_name': 'Task 4'},
-    dag=dag
-)
+    task_5 = PythonOperator(
+        task_id='task_5',
+        python_callable=task_function,
+        op_kwargs={'task_name': 'Task 5'}
+    )
 
-task_5 = PythonOperator(
-    task_id='task_5',
-    python_callable=task_function,
-    op_kwargs={'task_name': 'Task 5'},
-    dag=dag
-)
-
-# Add tasks to the TaskGroup
-tg1.add(task_1)
-tg1.add(task_2)
-tg1.add(task_3)
-tg1.add(task_4)
-tg1.add(task_5)
-
-# Define the task dependencies within the group
-task_1 >> [task_2, task_3] >> task_4 >> task_5
+    # Define the task dependencies within the group
+    task_1 >> [task_2, task_3] >> task_4 >> task_5
 
 # Set up the overall DAG structure
 start >> tg1 >> end
