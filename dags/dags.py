a3143a50b933e2b45933f39908fed33fc1836d8d
Ashu <ashuhimself@outlook.com>
2024-10-07 02:15:16 +0530

    Lib

--- dags/dags.py
+++ dags/dags.py
@@ -1,10 +1,8 @@
-# File: dags/dags.py
 
 from airflow import DAG
 from datetime import datetime
-from uti.task_groups import create_tasks  # Import the task group function
+from uti.task_groups import create_tasks 
 
-# Define the DAG
 dag = DAG(
     dag_id='dynamic_task_group_dag', 
     start_date=datetime(2024, 10, 7), 
@@ -12,8 +10,6 @@ dag = DAG(
     catchup=False
 )
 
-# Create the task group by calling the imported function
 task_group = create_tasks()
 
-# This allows you to set the task group to the DAG
 task_group.dag = dag
